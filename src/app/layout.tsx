b7568324617a88fe6e8e8e614c1d42f779dae199
Nyaughh <aarinarora02@gmail.com>
2024-05-02 12:47:28 +0530

    sumika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'Overwatch WhatsApp Group Chat',
+    title: 'Sumika WhatsApp Group Chat',
     description: 'Join Now'
 }
 
